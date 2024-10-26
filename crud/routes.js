77c2d89ef666fde198ec8a3c5e89a34530e44f70
simanto848 <simanto848@gmail.com>
2024-10-26 22:06:38 +0600

    login and hash password

--- crud/routes.js
+++ crud/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const db = require('./dbConfig');
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get('/create', (req, res) => {
 });
 
 // Store some data in the database
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const { full_name, age, email, password } = req.body;
 
   if (!full_name || !age || !email || !password) {
@@ -23,20 +24,63 @@ router.post('/create', (req, res) => {
   }
 
   const uniqueId = new Date().getTime();
-  console.log('Unique Id: ', uniqueId);
+  const hashPassword = await bcrypt.hash(password, 10);
 
   // sql query, data, callback(err, result)
   db.query(
     'INSERT INTO users (id, full_name, age, email, password) VALUES (?, ?, ?, ?, ?)',
-    [uniqueId, full_name, age, email, password],
+    [uniqueId, full_name, age, email, hashPassword],
     (err, result) => {
       if (err) {
-        console.log('Something went wrong in the sql: ', err);
+        return res.json({ message: 'Something went wrong!' });
       } else {
-        console.log('Data inserted successfully');
+        return res.json({ message: 'User created successfully!' });
       }
     }
   );
+});
+
+// Get method show login route template
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Login route
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({ message: 'All fields are required!' });
+  }
+
+  try {
+    db.query(
+      'SELECT * FROM users WHERE email = ?',
+      [email],
+      async (err, result) => {
+        if (err) {
+          return res.json({ message: 'Something went wrong!' });
+        } else {
+          if (result.length === 0) {
+            return res.json({ message: 'Invalid credentials!' });
+          } else {
+            const comparePassword = await bcrypt.compare(
+              password,
+              result[0].password
+            );
+
+            if (comparePassword) {
+              return res.json({ message: 'User logged in successfully!' });
+            } else {
+              return res.json({ message: 'Invalid credentials!' });
+            }
+          }
+        }
+      }
+    );
+  } catch (error) {
+    return res.json({ message: 'Something went wrong!' });
+  }
 });
 
 module.exports = router;
